85c14aef56f0c144d88decefaddd36c142d1eb79
Vedant Rahane <71424483+vedant115@users.noreply.github.com>
2025-02-15 05:34:28 +0530

    Time: 0 ms (100%), Space: 10.1 MB (68.21%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.cpp
+++ 0048-rotate-image/0048-rotate-image.cpp
@@ -3,18 +3,14 @@ public:
     void rotate(vector<vector<int>>& matrix) {
         int n = matrix.size();
 
-        vector<vector<int>> rotated(n, vector(n, 0));
-
         for(int i=0; i<n; i++){
-            for(int j=0; j<n; j++){
-                rotated[j][n-i-1] = matrix[i][j];
+            for(int j=0; j<i; j++){
+                swap(matrix[i][j], matrix[j][i]);
             }
         }
 
         for(int i=0; i<n; i++){
-            for(int j=0; j<n; j++){
-                matrix[i][j] = rotated[i][j];
-            }
+            reverse(matrix[i].begin(), matrix[i].end());
         }
     }
 };
