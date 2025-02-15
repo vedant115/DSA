30b0a26e90eb68f179f6bd05a6b43d91a6ded5fd
Vedant Rahane <71424483+vedant115@users.noreply.github.com>
2025-02-15 06:09:52 +0530

    Time: 0 ms (100%), Space: 16.8 MB (7.21%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -1,15 +1,34 @@
 class Solution {
+private:
+    static bool compare(const vector<int>& a, const vector<int>& b){
+        return a[0] < b[0];
+    }
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
         int n = nums.size();
 
-        unordered_map<int, int> mp;
-        for(int i=0; i<n; i++){
-            int cmpt = target - nums[i];
-            if(mp.count(cmpt))
-                return {mp[cmpt], i};
-            mp[nums[i]] = i;
+        vector<vector<int>> numIdx(n, vector<int>(2, -1));
+        for (int i=0; i<n; i++){
+            numIdx[i][0] = nums[i];
+            numIdx[i][1] = i;
         }
+
+        sort(numIdx.begin(), numIdx.end(), compare);
+
+        int left = 0, right = n-1;
+        while(left < right){
+            int sum = numIdx[left][0] + numIdx[right][0];
+            if(sum == target){
+                return {numIdx[left][1], numIdx[right][1]};
+            }
+            else if(sum < target){
+                left++;
+            }
+            else{
+                right--;
+            }
+        }
+
         return {-1, -1};
     }
 };
