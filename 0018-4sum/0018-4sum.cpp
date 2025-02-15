752a9e31f9813a243cd999ee73ec1c382fb3c609
Vedant Rahane <71424483+vedant115@users.noreply.github.com>
2025-02-15 07:48:04 +0530

    Time: 15 ms (90.01%), Space: 17.2 MB (98.72%) - LeetHub

--- 0018-4sum/0018-4sum.cpp
+++ 0018-4sum/0018-4sum.cpp
@@ -2,24 +2,33 @@ class Solution {
 public:
     vector<vector<int>> fourSum(vector<int>& nums, int target) {
         int n = nums.size();
-        set<vector<int>> tripletSet;
+        vector<vector<int>> res;
+        sort(nums.begin(), nums.end());
 
         for(int i=0; i<n; i++){
+            if(i>0 && nums[i] == nums[i-1]) continue;
             for(int j=i+1; j<n; j++){
-                set<long long> hashSet;
-                for(int k=j+1; k<n; k++){
-                    long long sum = (long long)nums[i] + nums[j] + nums[k];
-                    long long cmpt = (long long)target - sum;
-                    if(hashSet.count(cmpt)){
-                        vector<int> triplet = {nums[i], nums[j], nums[k], (int)cmpt};
-                        sort(triplet.begin(), triplet.end());
-                        tripletSet.insert(triplet);
+                if(j>i+1 && nums[j] == nums[j-1]) continue;
+                int left = j+1, right = n-1;
+                while(left < right){
+                    long long sum = (long long)nums[i] + nums[j] + nums[left] + nums[right];
+                    if(sum == target){
+                        res.push_back({nums[i], nums[j], nums[left], nums[right]});
+                        left++;
+                        right--;
+                        while(left<right && nums[left] == nums[left-1]) left++;
+                        while(left<right && nums[right] == nums[right+1]) right--;
                     }
-                    hashSet.insert(nums[k]);
+                    else if(sum < target){
+                        left++;
+                    }
+                    else{
+                        right--;
+                    }
                 }
             }
         }
 
-        return vector<vector<int>>(tripletSet.begin(), tripletSet.end());
+        return res;
     }
 };
