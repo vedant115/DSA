04f79b2b4bee4ae4477ecf406d050631f4211760
Vedant Rahane <71424483+vedant115@users.noreply.github.com>
2025-03-01 06:57:23 +0530

    Time: 0 ms (100%), Space: 13 MB (87.59%) - LeetHub

--- 2551-apply-operations-to-an-array/2551-apply-operations-to-an-array.cpp
+++ 2551-apply-operations-to-an-array/2551-apply-operations-to-an-array.cpp
@@ -2,17 +2,15 @@ class Solution {
 public:
     vector<int> applyOperations(vector<int>& nums) {
         int n = nums.size(), idx = 0;
-        int i;
-        for(i=0; i<n-1; i++){
-            if(nums[i] == nums[i+1]){
+        
+        for(int i=0; i<n; i++){
+            if((i+1 < n) && (nums[i] == nums[i+1])){
                 nums[i] = 2*nums[i];
                 nums[i+1] = 0;
             }
 
-            if(nums[i] != 0) swap(nums[i], nums[idx]);
-            if(nums[idx] != 0) idx++;
+            if(nums[i] != 0) swap(nums[i], nums[idx++]);
         }
-        swap(nums[i], nums[idx]);
         return nums;
     }
 };
