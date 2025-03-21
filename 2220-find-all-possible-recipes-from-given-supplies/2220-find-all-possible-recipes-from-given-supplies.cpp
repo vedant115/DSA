a6c075fb4cb2210641caa2cd61c97101cd39e29e
Vedant Rahane <71424483+vedant115@users.noreply.github.com>
2025-03-21 11:22:35 +0530

    Time: 44 ms (95.02%), Space: 107.8 MB (75.28%) - LeetHub

---
+++ 2220-find-all-possible-recipes-from-given-supplies/2220-find-all-possible-recipes-from-given-supplies.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    vector<string> findAllRecipes(vector<string>& recipes, vector<vector<string>>& ingredients, vector<string>& supply) {
+        unordered_set<string> supplies(supply.begin(), supply.end());
+
+        unordered_map<string, vector<string>> DAG;
+        unordered_map<string, int> inDegree;
+        for(auto& recipe : recipes){
+            inDegree[recipe] = 0;
+        }
+
+        for(int i=0; i<recipes.size(); i++){
+            for(int j=0; j<ingredients[i].size(); j++){
+                if(supplies.find(ingredients[i][j]) == supplies.end()){
+                    DAG[ingredients[i][j]].push_back(recipes[i]);
+                    inDegree[recipes[i]]++;
+                }
+            }
+        }
+
+        queue<string> q;
+        for(auto& recipe : inDegree){
+            if(recipe.second == 0) q.push(recipe.first);
+        }
+
+        vector<string> res;
+        while(!q.empty()){
+            string recipe = q.front();
+            q.pop();
+            res.push_back(recipe);
+            for(auto& ingredient : DAG[recipe]){
+                inDegree[ingredient]--;
+                if(inDegree[ingredient] == 0) q.push(ingredient);
+            }
+        }
+
+        return res;
+    }
+};
