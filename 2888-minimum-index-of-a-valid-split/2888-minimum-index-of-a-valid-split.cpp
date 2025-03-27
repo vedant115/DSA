35d038af802cb066f297a93eacc1d37b9a5b04d3
Vedant Rahane <71424483+vedant115@users.noreply.github.com>
2025-03-27 09:34:57 +0530

    Time: 35 ms (53.69%), Space: 102.8 MB (27.05%) - LeetHub

---
+++ 2888-minimum-index-of-a-valid-split/2888-minimum-index-of-a-valid-split.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    int minimumIndex(vector<int>& nums) {
+        int n = nums.size();
+        unordered_map<int, int> freq;
+        for(int num : nums){
+            freq[num]++;
+        }
+        int dominant = -1;
+        for(auto& f : freq){
+            if(f.second * 2 > n){
+                dominant = f.first;
+                break;
+            }
+        }
+        //cout<<"Dominant - "<<dominant;
+        vector<int> countTillI(n, 0);
+        int count = 0;
+        for(int i=0; i<n; i++){
+            if(nums[i] == dominant){
+                count++;
+            }
+            countTillI[i] = count;
+        }
+        for(int i=0; i<n; i++){
+            if(countTillI[i]*2 > i+1 && (freq[dominant]-countTillI[i])*2 > n-i-1){
+                return i;
+            }
+        }
+        return -1;
+    }
+};
