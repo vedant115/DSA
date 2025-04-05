3b217ec57e883008bb0a768e41dc2d0584654c05
Vedant Rahane <71424483+vedant115@users.noreply.github.com>
2025-04-05 10:50:50 +0530

    Time: 113 ms (5.64%), Space: 48.5 MB (5%) - LeetHub

--- 1993-sum-of-all-subset-xor-totals/1993-sum-of-all-subset-xor-totals.cpp
+++ 1993-sum-of-all-subset-xor-totals/1993-sum-of-all-subset-xor-totals.cpp
@@ -1,15 +1,37 @@
 class Solution {
-private:
-    int solve(int i, vector<int>& nums, int currXOR){
-        if(i == nums.size()) return currXOR;
-
-        int include = solve(i+1, nums, nums[i]^currXOR);
-        int exclude = solve(i+1, nums, currXOR);
-
-        return include + exclude;
-    }
 public:
     int subsetXORSum(vector<int>& nums) {
-        return solve(0, nums, 0);
+        vector<vector<int>> subsets;
+        // Generate all of the subsets
+        generateSubsets(nums, 0, {}, subsets);
+
+        // Compute the XOR total for each subset and add to the result
+        int result = 0;
+        for (auto& subset : subsets) {
+            int subsetXORTotal = 0;
+            for (int num : subset) {
+                subsetXORTotal ^= num;
+            }
+            result += subsetXORTotal;
+        }
+        return result;
+    }
+
+private:
+    void generateSubsets(const vector<int>& nums, int index, vector<int> subset, vector<vector<int>>& subsets) {
+        // Base case: index reached end of nums
+        // Add the current subset to subsets
+        if (index == nums.size()) {
+            subsets.push_back(subset);
+            return;
+        }
+
+        // Generate subsets with nums[i]
+        subset.push_back(nums[index]);
+        generateSubsets(nums, index + 1, subset, subsets);
+        subset.pop_back();
+
+        // Generate subsets without nums[i]
+        generateSubsets(nums, index + 1, subset, subsets);
     }
 };
