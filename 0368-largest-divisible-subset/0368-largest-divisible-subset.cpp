77fc888923ad2df280563afcb86a9ec0c4998680
Vedant Rahane <71424483+vedant115@users.noreply.github.com>
2025-04-06 08:47:50 +0530

    Time: 2 ms (99.67%), Space: 12.7 MB (20.34%) - LeetHub

--- 0368-largest-divisible-subset/0368-largest-divisible-subset.cpp
+++ 0368-largest-divisible-subset/0368-largest-divisible-subset.cpp
@@ -1,40 +1,28 @@
 class Solution {
-private:
-    int helper(int i, vector<int>& nums, vector<int>& dp, vector<int>& prev) {
-        if (dp[i] != -1) return dp[i];
-        int maxLen = 1, bestPrev = -1;
-        for (int j = 0; j < i; ++j) {
-            if (nums[i] % nums[j] == 0) {
-                int len = helper(j, nums, dp, prev) + 1;
-                if (len > maxLen) {
-                    maxLen = len;
-                    bestPrev = j;
-                }
-            }
-        }
-        dp[i] = maxLen;
-        prev[i] = bestPrev;
-        return dp[i];
-    }
 public:
     vector<int> largestDivisibleSubset(vector<int>& nums) {
         sort(nums.begin(), nums.end());
         int n = nums.size();
-        vector<int> dp(n, -1), prev(n, -1);
-        int maxLen = 0, lastIndex = -1;
+        vector<int> dp(n, 1), prev(n, -1);
+        int maxLen = 1, maxIndex = 0;
 
-        for (int i = 0; i < n; ++i) {
-            int len = helper(i, nums, dp, prev);
-            if (len > maxLen) {
-                maxLen = len;
-                lastIndex = i;
+        for (int i = 1; i < n; ++i) {
+            for (int j = 0; j < i; ++j) {
+                if (nums[i] % nums[j] == 0 && dp[j] + 1 > dp[i]) {
+                    dp[i] = dp[j] + 1;
+                    prev[i] = j;
+                }
+            }
+            if (dp[i] > maxLen) {
+                maxLen = dp[i];
+                maxIndex = i;
             }
         }
 
         vector<int> result;
-        while (lastIndex != -1) {
-            result.push_back(nums[lastIndex]);
-            lastIndex = prev[lastIndex];
+        while (maxIndex != -1) {
+            result.push_back(nums[maxIndex]);
+            maxIndex = prev[maxIndex];
         }
         reverse(result.begin(), result.end());
         return result;
