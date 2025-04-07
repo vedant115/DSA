7408a8fc00d14c6470377b54420dd109b7ee8ce7
Vedant Rahane <71424483+vedant115@users.noreply.github.com>
2025-04-07 12:49:15 +0530

    Time: 106 ms (67.08%), Space: 16.9 MB (69.09%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
@@ -1,26 +1,27 @@
 class Solution {
-private:
-    bool solve(int i, int sum, vector<int>& nums, vector<vector<int>>& dp){
-        if(sum == 0) return true;
-        if(i == nums.size()) return false;
-
-        if(dp[i][sum] != -1) return dp[i][sum];
-
-        bool include = false;
-        if(sum >= nums[i])
-            include = solve(i+1, sum-nums[i], nums, dp);
-
-        bool exclude = solve(i+1, sum, nums, dp);
-
-        return dp[i][sum] = include || exclude;
-    }
-
 public:
     bool canPartition(vector<int>& nums) {
+        int n = nums.size();
         int total = 0;
+
         for(int num : nums) total += num;
         if(total%2 == 1) return false;
-        vector<vector<int>> dp(nums.size(), vector<int>((total/2)+1, -1));
-        return solve(0, total/2, nums, dp);
+        int reqSum = total/2;
+
+        vector<vector<bool>> dp(nums.size()+1, vector<bool>((total/2)+1, false));
+        for(int i=0; i<=n; i++){
+            dp[i][0] = true;
+        }
+        for(int i=1; i<=n; i++){
+            for(int sum=0; sum<=reqSum; sum++){
+                if(sum >= nums[i-1]){
+                    dp[i][sum] = dp[i-1][sum] || dp[i-1][sum - nums[i-1]];
+                } else {
+                    dp[i][sum] = dp[i-1][sum];
+                }
+            }
+        }
+        
+        return dp[n][reqSum];
     }
 };
