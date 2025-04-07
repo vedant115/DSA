ed87cc213bb6d099c4bec637ebd06b6e99f22cb0
Vedant Rahane <71424483+vedant115@users.noreply.github.com>
2025-04-07 12:51:03 +0530

    Time: 75 ms (80.86%), Space: 13.4 MB (86.66%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
@@ -1,27 +1,21 @@
 class Solution {
 public:
     bool canPartition(vector<int>& nums) {
-        int n = nums.size();
-        int total = 0;
+        int total = accumulate(nums.begin(), nums.end(), 0);
+        if (total & 1) return false;
+        int reqSum = total / 2;
 
-        for(int num : nums) total += num;
-        if(total%2 == 1) return false;
-        int reqSum = total/2;
+        // dp[s] == true if we can make sum 's' using some of the numbers seen so far
+        vector<bool> dp(reqSum+1, false);
+        dp[0] = true;  // zero sum is always possible (take no elements)
 
-        vector<vector<bool>> dp(nums.size()+1, vector<bool>((total/2)+1, false));
-        for(int i=0; i<=n; i++){
-            dp[i][0] = true;
-        }
-        for(int i=1; i<=n; i++){
-            for(int sum=0; sum<=reqSum; sum++){
-                if(sum >= nums[i-1]){
-                    dp[i][sum] = dp[i-1][sum] || dp[i-1][sum - nums[i-1]];
-                } else {
-                    dp[i][sum] = dp[i-1][sum];
-                }
+        for (int num : nums) {
+            // iterate backwards so that each num is only used once
+            for (int s = reqSum; s >= num; --s) {
+                dp[s] = dp[s] || dp[s - num];
             }
         }
-        
-        return dp[n][reqSum];
+
+        return dp[reqSum];
     }
 };
