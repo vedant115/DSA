d4a7d16752ce4ccadea34341e7021dcfee830f58
Vedant Rahane <71424483+vedant115@users.noreply.github.com>
2025-04-14 14:15:38 +0530

    Time: 4 ms (90.51%), Space: 11.6 MB (5.91%) - LeetHub

--- 1656-count-good-triplets/1656-count-good-triplets.cpp
+++ 1656-count-good-triplets/1656-count-good-triplets.cpp
@@ -1,17 +1,29 @@
 class Solution {
 public:
     int countGoodTriplets(vector<int>& arr, int a, int b, int c) {
-        int n = arr.size(), cnt = 0;
-        for(int i=0; i<n-2; i++){
-            for(int j=i+1; j<n-1; j++){
-                for(int k=j+1; k<n; k++){
-                    if(abs(arr[i]-arr[j]) <= a && abs(arr[j]-arr[k]) <= b && 
-                        abs(arr[i]-arr[k]) <= c){
-                        cnt++;
+        int n = arr.size();
+        vector<int> prefixCnt(1001, 0);
+
+        int res = 0;
+        for(int j=0; j<n-1; j++){
+            for(int k=j+1; k<n; k++){
+                if(abs(arr[j]-arr[k]) <= b){
+                    int lj = arr[j] - a, lk = arr[k] - c;
+                    int rj = arr[j] + a, rk = arr[k] + c;
+                    int l = max(0, max(lj, lk)), r = min(1000, min(rj, rk));
+
+                    if(l <= r){
+                        if(l == 0)
+                            res += prefixCnt[r];
+                        else
+                            res += prefixCnt[r] - prefixCnt[l-1];
                     }
                 }
             }
+            for(int num = arr[j]; num<1001; num++){
+                prefixCnt[num]++;
+            }
         }
-        return cnt;
+        return res;
     }
 };
