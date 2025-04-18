b4b93ec39c8b59f70040ef57306885b738731abe
Vedant Rahane <71424483+vedant115@users.noreply.github.com>
2025-04-18 11:42:58 +0530

    Time: 6 ms (34.47%), Space: 9.6 MB (91.45%) - LeetHub

--- 0038-count-and-say/0038-count-and-say.cpp
+++ 0038-count-and-say/0038-count-and-say.cpp
@@ -1,25 +1,25 @@
 class Solution {
-private:
-    string computeRLE(string s) {
-        string ans = "";
-        int i = 0;
-        while (i < s.length()) {
-            int count = 1;
-            while (i + 1 < s.length() && s[i] == s[i + 1]) {
-                i++;
-                count++;
-            }
-            ans += to_string(count);
-            ans += s[i];
-            i++;
-        }
-        return ans;
-    }
 public:
     string countAndSay(int n) {
         if (n == 1) return "1";
 
-        string temp = countAndSay(n - 1);
-        return computeRLE(temp);
+        string s = "1";
+        string ans = "";
+        for(int ch=2; ch<=n; ch++){
+            int i = 0;
+            while (i < s.length()) {
+                int count = 1;
+                while (i + 1 < s.length() && s[i] == s[i + 1]) {
+                    i++;
+                    count++;
+                }
+                ans += to_string(count);
+                ans += s[i];
+                i++;
+            }
+            s = ans;
+            ans = "";
+        }
+        return s;
     }
 };
