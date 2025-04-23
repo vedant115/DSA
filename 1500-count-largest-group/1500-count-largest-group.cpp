cc7acf79d893581bde3bfa0daf8f5b66741bdf8c
Vedant Rahane <71424483+vedant115@users.noreply.github.com>
2025-04-23 22:10:45 +0530

    Time: 2 ms (49.15%), Space: 8.1 MB (57.97%) - LeetHub

---
+++ 1500-count-largest-group/1500-count-largest-group.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int countLargestGroup(int n) {
+        int maxSoFar = 0, res = 0;
+        unordered_map<int, int> mpp;
+        for(int i=1; i<=n; i++){
+            int sum = 0, num = i;
+            while(num){
+                sum += (num%10);
+                num /= 10;
+            }
+            mpp[sum]++;
+            if(maxSoFar < mpp[sum]){
+                maxSoFar = mpp[sum];
+                res = 1;
+            }
+            else if(maxSoFar == mpp[sum]) res++;
+        }
+        return res;
+    }
+};
