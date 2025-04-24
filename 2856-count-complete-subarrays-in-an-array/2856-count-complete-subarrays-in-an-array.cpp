b547a88acdc4c0bacfaf6d68628a0a008680fcb4
Vedant Rahane <71424483+vedant115@users.noreply.github.com>
2025-04-24 08:45:47 +0530

    Time: 1512 ms (14.19%), Space: 483.5 MB (8.97%) - LeetHub

--- 2856-count-complete-subarrays-in-an-array/2856-count-complete-subarrays-in-an-array.cpp
+++ 2856-count-complete-subarrays-in-an-array/2856-count-complete-subarrays-in-an-array.cpp
@@ -1,29 +1,19 @@
 class Solution {
-private:
-    int countAtMostK(vector<int>& nums, int k) {
-        if (k == 0) return 0;
-        unordered_map<int, int> freq;
-        int left = 0, right = 0, res = 0;
-        
-        while (right < nums.size()) {
-            freq[nums[right]]++;
-            while (freq.size() > k) {
-                freq[nums[left]]--;
-                if (freq[nums[left]] == 0) {
-                    freq.erase(nums[left]);
-                }
-                left++;
-            }
-            res += (right - left + 1);
-            right++;
-        }
-        return res;
-    }
 public:
     int countCompleteSubarrays(vector<int>& nums) {
         unordered_set<int> totalSet(nums.begin(), nums.end());
         int totalDistinct = totalSet.size();
+        int res = 0;
 
-        return countAtMostK(nums, totalDistinct) - countAtMostK(nums, totalDistinct - 1);
+        for (int i = 0; i < nums.size(); ++i) {
+            unordered_set<int> currSet;
+            for (int j = i; j < nums.size(); ++j) {
+                currSet.insert(nums[j]);
+                if (currSet.size() == totalDistinct) {
+                    res++;
+                }
+            }
+        }
+        return res;
     }
 };
