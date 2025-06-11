9a5933f9dbf8c3effd2fd5204b00d37d67d991e5
Vedant Rahane <71424483+vedant115@users.noreply.github.com>
2025-06-11 12:18:43 +0530

    Time: 7 ms (46.46%), Space: 16.7 MB (7.22%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -1,30 +1,28 @@
 class Solution {
-private:
-    static bool compare(const vector<int>& a, const vector<int>& b){
-        return a[0] < b[0];
-    }
+
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
         int n = nums.size();
 
         vector<vector<int>> numIdx(n, vector<int>(2, -1));
-        for (int i=0; i<n; i++){
+        for (int i = 0; i < n; i++) {
             numIdx[i][0] = nums[i];
             numIdx[i][1] = i;
         }
 
-        sort(numIdx.begin(), numIdx.end(), compare);
+        sort(numIdx.begin(), numIdx.end(),
+             [](const vector<int>& a, const vector<int>& b) {
+                 return a[0] < b[0];
+             });
 
-        int left = 0, right = n-1;
-        while(left < right){
+        int left = 0, right = n - 1;
+        while (left < right) {
             int sum = numIdx[left][0] + numIdx[right][0];
-            if(sum == target){
+            if (sum == target) {
                 return {numIdx[left][1], numIdx[right][1]};
-            }
-            else if(sum < target){
+            } else if (sum < target) {
                 left++;
-            }
-            else{
+            } else {
                 right--;
             }
         }
