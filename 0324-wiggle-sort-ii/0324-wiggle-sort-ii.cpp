dd7e3e454803451a8522c964db24a6f17fdee547
Vedant Rahane <71424483+vedant115@users.noreply.github.com>
2025-06-12 14:10:42 +0530

    Time: 0 ms (100%), Space: 21.4 MB (95.18%) - LeetHub

--- 0324-wiggle-sort-ii/0324-wiggle-sort-ii.cpp
+++ 0324-wiggle-sort-ii/0324-wiggle-sort-ii.cpp
@@ -2,23 +2,27 @@ class Solution {
 public:
     void wiggleSort(vector<int>& nums) {
         int n = nums.size();
-        sort(nums.begin(), nums.end());
-        vector<int> res(n);
 
-        int startPtr = 0, endPtr = n-1;
-        if(n%2) startPtr = (n/2);
-        else startPtr = (n/2)-1;
-        
-        for(int i=0; i<n; i++){
-            if(i % 2 == 0){
-                res[i] = nums[startPtr];
-                startPtr--;
-            }
-            else{
-                res[i] = nums[endPtr];
-                endPtr--;
+        // Step 1: Find the median
+        auto mid = nums.begin() + n / 2;
+        nth_element(nums.begin(), mid, nums.end());
+        int median = *mid;
+
+        // Virtual indexing lambda
+        auto index = [n](int i) { return (1 + 2 * i) % (n | 1); };
+
+        // 3-way partition (Dutch National Flag)
+        int i = 0, j = 0, k = n - 1;
+        while (j <= k) {
+            if (nums[index(j)] > median) {
+                swap(nums[index(i)], nums[index(j)]);
+                i++; j++;
+            } else if (nums[index(j)] < median) {
+                swap(nums[index(j)], nums[index(k)]);
+                k--;
+            } else {
+                j++;
             }
         }
-        nums = res;
     }
 };
