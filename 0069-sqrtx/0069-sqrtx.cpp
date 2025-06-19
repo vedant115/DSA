f5bf4ac84310247cf3dc0ec3117439eb1ec74323
Vedant Rahane <71424483+vedant115@users.noreply.github.com>
2025-06-19 11:36:34 +0530

    Time: 0 ms (100%), Space: 8.6 MB (50.88%) - LeetHub

---
+++ 0069-sqrtx/0069-sqrtx.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int mySqrt(int x) {
+        long long low = 1, high = x;
+
+        while(low <= high){
+            long long mid = low + (high-low)/2;
+            long long val = mid * mid;
+            if(val <= x){
+                low = mid + 1;
+            }
+            else{
+                high = mid - 1;
+            }
+        }
+        return high;
+    }
+};
