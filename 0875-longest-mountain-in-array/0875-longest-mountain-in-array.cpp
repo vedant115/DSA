a40877c0cfc78b5d3476f6d9f689bd8cc9f9941e
Vedant Rahane <71424483+vedant115@users.noreply.github.com>
2025-06-25 11:52:38 +0530

    Time: 0 ms (100%), Space: 22.2 MB (95.72%) - LeetHub

--- 0875-longest-mountain-in-array/0875-longest-mountain-in-array.cpp
+++ 0875-longest-mountain-in-array/0875-longest-mountain-in-array.cpp
@@ -1,22 +1,21 @@
 class Solution {
 public:
-    int longestMountain(vector<int>& arr) {
-        int n = arr.size();
-        vector<int> up(n, 0), down(n, 0);
-        
-        for (int i = 1; i < n; i++)
-            if (arr[i] > arr[i - 1])
-                up[i] = up[i - 1] + 1;
-        
-        for (int i = n - 2; i >= 0; i--)
-            if (arr[i] > arr[i + 1])
-                down[i] = down[i + 1] + 1;
-        
-        int max_len = 0;
-        for (int i = 0; i < n; i++) {
-            if (up[i] > 0 && down[i] > 0)
-                max_len = max(max_len, up[i] + down[i] + 1);
+    int longestMountain(vector<int>& A) {
+        int res = 0, up = 0, down = 0;
+        for (int i = 1; i < A.size(); ++i) {
+            // If we are going down and current element is greater than prev
+            // MEANS last mountain ended And the current mountain began (Up will
+            // be updated below)
+            if (down > 0 && A[i - 1] < A[i] || A[i - 1] == A[i])
+                up = down = 0;
+
+            // If current element is greater then previous then we are going up
+            // Else we are going down the mountain
+            A[i] > A[i - 1] ? up++ : down++;
+
+            if (up > 0 && down > 0)
+                res = max(res, up + down + 1);
         }
-        return max_len;
+        return res;
     }
 };
