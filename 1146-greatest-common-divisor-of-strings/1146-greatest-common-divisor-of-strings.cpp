e08b0c5791f920a8f5b4991839a8dec67112a9de
Vedant Rahane <71424483+vedant115@users.noreply.github.com>
2025-07-03 09:10:45 +0530

    Time: 0 ms (100%), Space: 9.7 MB (57.58%) - LeetHub

--- 1146-greatest-common-divisor-of-strings/1146-greatest-common-divisor-of-strings.cpp
+++ 1146-greatest-common-divisor-of-strings/1146-greatest-common-divisor-of-strings.cpp
@@ -1,29 +1,10 @@
 class Solution {
 public:
     string gcdOfStrings(string str1, string str2) {
-        int n1 = str1.size(), n2 = str2.size();
-        if(n2 < n1){
-            swap(str1, str2);
-        }
-        string res = "";
-        for(int i=1; i<=str1.size(); i++){
-            string substr = str1.substr(0, i);
-            int flag = 0;
-            for(int j=0; j<str2.size(); j+=i){
-                if(substr != str2.substr(j, i)){
-                    flag = 1;
-                    break;
-                }
-            }
-            if(flag == 1) continue;
-            for(int k=0; k<str1.size(); k+=i){
-                if(substr != str1.substr(k, i)){
-                    flag = 1;
-                    break;
-                }
-            }
-            if(flag == 0) res = substr;
-        }
-        return res;
+        // Check if concatenated strings are equal or not, if not return ""
+        if(str1 + str2 != str2 + str1)
+            return "";
+        // If strings are equal than return the substring from 0 to gcd of size(str1), size(str2)
+        return str1.substr(0, gcd(str1.size(), str2.size()));
     }
 };
