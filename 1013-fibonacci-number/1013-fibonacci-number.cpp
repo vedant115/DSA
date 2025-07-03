7772e8087ddfa99c21ccd859d5d4a57a18c3ca70
Vedant Rahane <71424483+vedant115@users.noreply.github.com>
2025-07-03 08:10:16 +0530

    Time: 2 ms (77.46%), Space: 7.8 MB (36.52%) - LeetHub

--- 1013-fibonacci-number/1013-fibonacci-number.cpp
+++ 1013-fibonacci-number/1013-fibonacci-number.cpp
@@ -1,11 +1,23 @@
 class Solution {
 public:
+    static vector<int> dp; 
+    Solution() {
+        if(dp.empty()){
+            dp.resize(31, -1);
+            dp[0] = 0;
+            dp[1] = 1;
+        }
+    }
     int fib(int n) {
         if(n <= 1){
-            return n;
+            return dp[n];
+        }
+        else if(dp[n] != -1){
+            return dp[n];
         }
         else{
-            return fib(n-1) + fib(n-2);
+            return dp[n] = fib(n-1) + fib(n-2);
         }
     }
 };
+vector<int> Solution::dp;
