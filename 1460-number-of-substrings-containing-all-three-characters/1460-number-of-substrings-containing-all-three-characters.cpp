b4024e096f8c7c60dc2cf95f71714d55e695bfcb
Vedant Rahane <71424483+vedant115@users.noreply.github.com>
2025-07-15 12:10:12 +0530

    Time: 55 ms (5.17%), Space: 11.1 MB (56.1%) - LeetHub

--- 1460-number-of-substrings-containing-all-three-characters/1460-number-of-substrings-containing-all-three-characters.cpp
+++ 1460-number-of-substrings-containing-all-three-characters/1460-number-of-substrings-containing-all-three-characters.cpp
@@ -1,12 +1,26 @@
 class Solution {
 public:
     int numberOfSubstrings(string s) {
-        vector<int> lastSeen(3, -1);
-        int res = 0;
-        for(int i=0; i<s.size(); i++){
-            char ch = s[i];
-            lastSeen[ch - 'a'] = i;
-            res += (min({lastSeen[0], lastSeen[1], lastSeen[2]}) + 1);
+        int l = 0, r = 0, res = 0;
+        unordered_map<char, int> freq;
+        while(r < s.size()){
+            freq[s[r]]++;
+            while(freq['a'] >= 1 && freq['b'] >= 1 && freq['c'] >= 1){
+                if(freq[s[l]] > 1){
+                    freq[s[l]]--;
+                    l++;  
+                }
+                else{
+                    break;
+                }
+                if(freq['a'] == 1 && freq['b'] == 1 && freq['c'] == 1){
+                    break;
+                }
+            }
+            if(freq['a'] >= 1 && freq['b'] >= 1 && freq['c'] >= 1){
+                res += (l + 1);
+            }
+            r++;
         }
         return res;
     }
