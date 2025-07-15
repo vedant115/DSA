1c4643f019e4dc7119a24af8c415ce3710f8f535
Vedant Rahane <71424483+vedant115@users.noreply.github.com>
2025-07-15 17:44:34 +0530

    Time: 80 ms (12.57%), Space: 94.3 MB (16.68%) - LeetHub

--- 1370-count-number-of-nice-subarrays/1370-count-number-of-nice-subarrays.cpp
+++ 1370-count-number-of-nice-subarrays/1370-count-number-of-nice-subarrays.cpp
@@ -1,19 +1,16 @@
 class Solution {
 public:
-    int func(vector<int>& nums, int k){
-        int r = 0, l = 0, noOdd = 0, cnt = 0;
-        while(r < nums.size()){
-            if(nums[r]%2) noOdd++;
-            while(noOdd > k){
-                if(nums[l]%2) noOdd--;
-                l++;
-            }
-            cnt += (r-l+1);
-            r++;
-        }
-        return cnt;
-    }
     int numberOfSubarrays(vector<int>& nums, int k) {
-        return func(nums, k) - func(nums, k-1);
+        unordered_map<int, int> oddFreq;
+        int currOdds = 0, res = 0;
+        oddFreq[0] = 1;
+        for(int num : nums){
+            if(num%2) currOdds++;
+            if(oddFreq.find(currOdds - k) != oddFreq.end()){
+                res += oddFreq[currOdds - k];
+            }
+            oddFreq[currOdds]++;
+        }
+        return res;
     }
 };
