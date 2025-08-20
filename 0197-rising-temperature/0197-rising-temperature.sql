28b3b7c13b2a9a70a4898257ec0810d7832951cf
Vedant Rahane <71424483+vedant115@users.noreply.github.com>
2025-08-20 20:10:04 +0530

    Time: 502 ms (43.39%), Space: 0B (100%) - LeetHub

--- 0197-rising-temperature/0197-rising-temperature.sql
+++ 0197-rising-temperature/0197-rising-temperature.sql
@@ -1,4 +1,10 @@
-SELECT t1.id 
-FROM Weather t1
-JOIN Weather t2
-WHERE DATEDIFF(t1.recordDate, t2.recordDate) = 1 AND t1.temperature > t2.temperature;
+SELECT 
+    w1.id
+FROM 
+    Weather w1
+JOIN 
+    Weather w2
+ON 
+    DATEDIFF(w1.recordDate, w2.recordDate) = 1
+WHERE 
+    w1.temperature > w2.temperature;
