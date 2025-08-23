6437a3cbcacca173f914b7759c2deb35aa322296
Vedant Rahane <71424483+vedant115@users.noreply.github.com>
2025-08-23 18:03:37 +0530

    Time: 563 ms (43.28%), Space: 0B (100%) - LeetHub

--- 1942-primary-department-for-each-employee/1942-primary-department-for-each-employee.sql
+++ 1942-primary-department-for-each-employee/1942-primary-department-for-each-employee.sql
@@ -1,9 +1,19 @@
-SELECT employee_id, department_id
-FROM Employee
-WHERE primary_flag = 'Y' OR employee_id IN 
-    (
-        SELECT employee_id
-        FROM Employee
-        GROUP BY employee_id
-        HAVING COUNT(*) = 1
-   );
+-- Retrieving employees with primary_flag set to 'Y'
+SELECT 
+  employee_id, 
+  department_id 
+FROM 
+  Employee 
+WHERE 
+  primary_flag = 'Y' 
+UNION 
+-- Retrieving employees that appear exactly once in the Employee table
+SELECT 
+  employee_id, 
+  department_id 
+FROM 
+  Employee 
+GROUP BY 
+  employee_id 
+HAVING 
+  COUNT(employee_id) = 1;
