2f0ed8ea03877219ba7de8b1c13256901a85e75e
Vedant Rahane <71424483+vedant115@users.noreply.github.com>
2025-08-23 11:32:45 +0530

    Time: 487 ms (98.86%), Space: 0B (100%) - LeetHub

--- 1182-game-play-analysis-iv/1182-game-play-analysis-iv.sql
+++ 1182-game-play-analysis-iv/1182-game-play-analysis-iv.sql
@@ -1,10 +1,9 @@
-SELECT 
-    ROUND(COUNT(DISTINCT a1.player_id) / (SELECT COUNT(DISTINCT player_id) FROM Activity), 2) AS fraction
-FROM Activity a
-JOIN (
-    SELECT player_id, MIN(event_date) AS first_login
-    FROM Activity
-    GROUP BY player_id
-) a1
-  ON a.player_id = a1.player_id
-WHERE DATEDIFF(a.event_date, a1.first_login) = 1;
+SELECT
+  ROUND(COUNT(A1.player_id)/ (SELECT COUNT(DISTINCT A3.player_id) FROM Activity A3), 2) AS fraction
+FROM
+  Activity A1
+WHERE (A1.player_id, DATE_SUB(A1.event_date, INTERVAL 1 DAY)) IN 
+    (SELECT A2.player_id, MIN(A2.event_date)
+    FROM Activity A2
+    GROUP BY A2.player_id
+    );
