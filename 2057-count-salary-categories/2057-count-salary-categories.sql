34d5f74f06bdf214187e33b74718ff223ff939b0
Vedant Rahane <71424483+vedant115@users.noreply.github.com>
2025-08-23 22:57:20 +0530

    Time: 1636 ms (40.51%), Space: 0B (100%) - LeetHub

--- 2057-count-salary-categories/2057-count-salary-categories.sql
+++ 2057-count-salary-categories/2057-count-salary-categories.sql
@@ -1,14 +1,8 @@
-SELECT c.category, 
-       COALESCE(COUNT(a.account_id), 0) AS accounts_count
-FROM (
-    SELECT "Low Salary" AS category
-    UNION
-    SELECT "Average Salary"
-    UNION
-    SELECT "High Salary"
-) c
-LEFT JOIN Accounts a
-ON (c.category = "Low Salary" AND a.income < 20000)
-   OR (c.category = "Average Salary" AND a.income BETWEEN 20000 AND 50000)
-   OR (c.category = "High Salary" AND a.income > 50000)
-GROUP BY c.category;
+SELECT 'Low Salary' AS category, SUM(CASE WHEN income < 20000 THEN 1 ELSE 0 END) AS accounts_count
+FROM Accounts
+UNION
+SELECT 'Average Salary', SUM(CASE WHEN income BETWEEN 20000 AND 50000 THEN 1 ELSE 0 END)
+FROM Accounts
+UNION
+SELECT 'High Salary', SUM(CASE WHEN income > 50000 THEN 1 ELSE 0 END)
+FROM Accounts;
