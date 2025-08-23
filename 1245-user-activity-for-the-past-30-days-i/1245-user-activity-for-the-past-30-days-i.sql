6d2f83519c28c25e2b776cba0b94b5a7d2d34998
Vedant Rahane <71424483+vedant115@users.noreply.github.com>
2025-08-23 11:44:24 +0530

    Time: 436 ms (95.84%), Space: 0B (100%) - LeetHub

--- 1245-user-activity-for-the-past-30-days-i/1245-user-activity-for-the-past-30-days-i.sql
+++ 1245-user-activity-for-the-past-30-days-i/1245-user-activity-for-the-past-30-days-i.sql
@@ -1,4 +1,4 @@
-SELECT activity_date day, COUNT(DISTINCT user_id) active_users 
+SELECT activity_date AS day, COUNT(DISTINCT user_id) AS active_users
 FROM Activity
-WHERE activity_date > DATE_SUB('2019-07-27', INTERVAL 30 DAY) AND activity_date <= '2019-07-27'
-GROUP BY day;
+WHERE DATEDIFF('2019-07-27', activity_date) < 30 AND DATEDIFF('2019-07-27', activity_date)>=0
+GROUP BY day
